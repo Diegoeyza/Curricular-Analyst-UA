ed42e8978776ee2607e3222273330e9734e75535
Kylar <diegoeyzaguirreb@gmail.com>
2024-12-17 09:43:15 -0300

    added css styling

--- Analisis_curricular/django_app/RA_query_app/RA_query_app/settings.py
+++ Analisis_curricular/django_app/RA_query_app/RA_query_app/settings.py
@@ -147,7 +147,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# For development purposes, if you're using the default configuration:
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
